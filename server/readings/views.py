cbe9b42eec9c6f3c48ea7b3b5fc6660b84955acd
nishant-sachdeva <nishant.sachdeva@research.iiit.ac.in>
2019-11-03 20:35:54 +0530

    changes

--- server/readings/views.py
+++ server/readings/views.py
@@ -5,15 +5,33 @@ from django.utils import timezone
 # here we will make our views from  the given data
 # I am guessing we will be receiving data using the admin thing
 # then we import it here, and the redirect it to our views I guess
-from .models import Reading, Time
+from .models import Reading, Time, Identity
 
 import json
 
+import datetime
+import pytz 
+
 # the functions get defined  here
 
-now = timezone.now()
-
 def checkreq(request):
+	'''
+	so here's the deal, this guy takes in the request and works on it
+	it loads the value and status from the json object that has been received
+	makes a reading object based on that
+	saves it
+
+	no, if the status is 1, that means it has been  10 more minutes since the last reading, hence we add 1/6 ht of an hour to the total time
+
+	now we update the identiity object to store the identity of the lastest object
+	this identity will keep updating with every new object
+
+	so at the end of the function . we have the new data in a new object , we have the latest id and we have the totol running time for upto now
+
+	this all will be there in the data base for further use
+	'''
+
+
 	x = 0
 	y = -1
 	if request.method == "POST":
@@ -21,57 +39,68 @@ def checkreq(request):
 		x = json.loads(request.body)['status']
 		y = json.loads(request.body)['value']
 
-		now = timezone.now()
-		
-		obj = Reading(value = y , status = x)
 
-		obj.save()	
+		reading_obj = Reading(value = y , status = x)
+		reading_obj.save()	
 
 		# now we update time 
 		if x == 1:
-			obj = Time.objects.get(id=1)
-			obj.total_time += float(1/6)
-			obj.save()
+			time_obj = Time.objects.get(id=1)
+			time_obj.total_time += float(1/6)
+			time_obj.save()
 
 		# now we update the identit
 		id_object = Identity.objects.get(id=1)
-		id_object.Identity  = obj.id
+		id_object.Identity  = reading_obj.id
 		id_object.save()
 		# so now  we have the identity of the new object
 
-		context = {
-			"list" : {
-				"value" : y,
-				"status" : x,
-				"time" : timezone.now(),
-				"text" : "ID just created is " + str(obj.id) , 
-			},
-		}
-		print(Identity)
 		return HttpResponse(status=201)
 	else:
 		return HttpResponse("<h1>GET request intercepted successfully</h1>")
 
 
 def home_view(request, *args, **kwargs):
-	# now = timezone.now()
+	'''
+	so here is the deal for this function, we have the latest id, in case that dosen't work, we will have the default base.html page to take care of our things, 
+
+	latest id retrieved:
+	latest object retrieved for the object
+	total time retrieved
+
+	make a context object for all these things
+
+	display on the final web page
+
+	'''
 	print(request)
 	print(Identity)
-	print(ab)
 	
 	id_object = Identity.objects.get(id=1)
 	id_to_be_showed = id_object.Identity
 
-	obj = Reading.objects.get(id = id_to_be_showed)
+	if id_to_be_showed == 1:
+		return render(request, "base.html", {})
 
+	obj = Reading.objects.get(id = id_to_be_showed)
 	val = obj.value
 	stat = obj.status
+
+	time_obj = Time.objects.get(id=1)
+
+	
+	current_time = pytz.utc.localize(datetime.datetime.utcnow())
+	now = current_time.astimezone(pytz.timezone("Asia/Kolkata"))
+	# now = timezone.now() 
+
 	context = {
 		"list" : {
 			"id":id_to_be_showed,
 			"value" :val,
 			"status" : stat,
-			"time" : now 
+			"time" : now ,
+			"total_time" : round(time_obj.total_time , 2),
+			"redirect_url" : "https://localhost:8000/"
 		},
 	}	
 	return render(request, "home.html", context)
